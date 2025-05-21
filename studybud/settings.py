d520ac74167324461ee086d1ccb6435ed0276f88
vsaidivya <vsaidivya03@gmail.com>
2025-05-21 23:40:51 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -78,7 +78,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],  
+            "hosts": [os.environ.get('REDIS_URL')],
         },
     },
 }
